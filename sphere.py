6849201db724345d72dd14f6ea8e1045afe674e5
ArtemisMucaj <artemis.mucaj@gmail.com>
2015-01-17 19:17:24 +0100

    fix sphere controller and modified vrep scene

--- sphere.py
+++ sphere.py
@@ -8,25 +8,25 @@ def sphere_controller(frequency, pos_robot):
 	if frequency >= 0 and frequency <= 0.33:
 		# toy moves randomly inside the map
 		# Map is 10x10 ... (depends on the map used)
-		return generate_random_position(10,10,pos_robot,0.5)
+		return generate_random_position(-0.7, -0.475,0.35,0.475,pos_robot,0.05)
 	elif frequency > 0.33 and frequency <= 0.66:
 		# stops moving
 		return 0
 	elif frequency > 0.66 and frequency <= 1:
 		# toy jumps on robot position (y axis is vertical axis)
-		return [pos_robot[0], 0.5 , pos_robot[1]]
+		return [pos_robot[0], pos_robot[1], 0.0]
 	else:
 		# frequency out of widthband
 		return 0
 
-def generate_random_position(max_width, max_length, pos_robot, radius):
+def generate_random_position(min_width, min_length,max_width, max_length, pos_robot, radius):
 	while True:
-		generated_pos=[random.uniform(0,max_width), 0.5, random.uniform(0,max_length)]
+		generated_pos=[random.uniform(min_width,max_width), random.uniform(min_length,max_length), 0]
 		if(isCollision(generated_pos, pos_robot, radius) == False):
 			return generated_pos
 
 def isCollision(generated_pos, pos_robot, radius):
-	if sqrt((generated_pos[0] - pos_robot[0])**2 + (generated_pos[2] - pos_robot[1])**2) <= radius:
+	if sqrt((generated_pos[0] - pos_robot[0])**2 + (generated_pos[1] - pos_robot[1])**2) <= radius:
 		return True
 	else:
 		return False
